999add92e3cfde6d62553ad462c3852f9aba12c5
yveska <adamarshad.school@gmail.com>
2025-10-11 18:35:48 -0400

    Add files via upload

---
+++ Arch Linux (Rh)/config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "webhooked/kanso.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanso-zen",
+    },
+  },
+}
