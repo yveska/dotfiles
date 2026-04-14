9188e51a4dd0962a6aa85c3016b0906e69d3d30d
Adam <adamarshad.school@gmail.com>
2026-04-14 00:39:50 -0400

    update: 2026-04-14_00:39

--- install_linux.sh
+++ install_linux.sh
@@ -6,7 +6,7 @@ DOTFILES_DIR="$HOME/dotfiles"
 
 CONFIGS=(bash doom dunst fastfetch gtk-3.0 gtk-4.0 hypr icons kitty mpd rmpc nvim starship themes waybar)
 
-SERVICES=(NetworkManager bluetooth sddm)
+SERVICES=(NetworkManager bluetooth sddm mpd)
 
 # Native Packages
 
